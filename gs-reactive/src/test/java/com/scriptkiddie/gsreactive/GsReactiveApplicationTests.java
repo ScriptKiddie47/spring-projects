efbfa09a91f378b23e154658ab9804ea42ad1567
ScriptKiddie47 <ritambala@live.com>
2023-03-24 00:19:50 +0530

    reactive-project

---
+++ gs-reactive/src/test/java/com/scriptkiddie/gsreactive/GsReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scriptkiddie.gsreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
